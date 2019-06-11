fbc87df5827e778c3540b6effd63fa6bdaaedc30
DarksKnight <s20977372@gmail.com>
2019-06-11 17:11:26 +0800

    Update global.js

--- src/renderer/store/modules/global.js
+++ src/renderer/store/modules/global.js
@@ -1,27 +1,42 @@
 const state = {
-  accountInfo: null,
-  starTotalCount: null,
-  starItems: null
+  firstCursor: null,
+  starItems: null,
+  totalCount: null
 }
 
 const mutations = {
-  SET_ACCOUNT_INFO(state, data) {
-    state.accountInfo = data;
-  },
   SET_STAR_ITEMS(state, data) {
     state.starItems = data;
+  },
+  CLEAR_STAR_ITEMS(state) {
+    state.starItems = null;
+  },
+  SET_FIRST_CURSOR(state, data) {
+    state.firstCursor = data;
+  },
+  CLEAR_FIRST_CURSOR(state) {
+    state.firstCursor = null;
+  },
+  SET_TOTAL_COUNT(state, data) {
+    state.totalCount = data;
   }
 }
 
 const actions = {
-  setAccountInfo({ commit }, data) {
-    commit('SET_ACCOUNT_INFO', data);
-    return new Promise((resolve, reject) => {
-      resolve();
-    })
-  },
   setStarItems({ commit }, data) {
     commit('SET_STAR_ITEMS', data);
+  },
+  clearStarItems({ commit }) {
+    commit('CLEAR_STAR_ITEMS')
+  },
+  setFirstCursor({ commit }, data) {
+    commit('SET_FIRST_CURSOR', data);
+  },
+  clearFirstCursor({ commit }) {
+    commit('CLEAR_FIRST_CURSOR');
+  },
+  setTotalCount({ commit }, data) {
+    commit('SET_TOTAL_COUNT', data);
   }
 }
 
